962f49fbd57d5b99f360e1660edb43fc058bdd04
Ken Odegard <kodegard@anaconda.com>
2022-01-13 15:46:59 -0600

    Initial issue-in-project action (#4)
    * Initial issue-in-project action
    
    * Drop version from README
    
    * Update path trigger
    
    * Remove prettier from build
    
    * Exclude dist from pre-commit
    
    * Use conda-bot as rebuild author
    
    * Rebuild issue-in-project
    
    Co-authored-by: Conda Bot <conda-bot@users.noreply.github.com>

---
+++ issue-in-project/src/cli.ts
@@ -0,0 +1,50 @@
+import { Command } from 'commander'
+import { Owner, issueInProject } from '@src/api'
+import { setFailed } from '@actions/core'
+
+/**
+ * Command line interface.
+ * Expects all inputs to be provided on the command line (see --help).
+ */
+export async function cli(
+  argv?: string[],
+  exceptionError?: boolean,
+): Promise<void> {
+  const program = new Command()
+    .name('issue-in-project')
+    .showSuggestionAfterError()
+    .showHelpAfterError()
+
+  if (exceptionError)
+    program.exitOverride(err => {
+      throw err
+    })
+
+  program
+    .description(
+      'detect if an issue is found within a given project, if no issue defined simply list all issues',
+    )
+    .option('-o, --org <org>', 'the organization to search (e.g. conda)')
+    .option('-u, --user <user>', 'the user to search (e.g. conda-bot)')
+    .option(
+      '-r, --repo, --repository <repo>',
+      'the repository to search (e.g. conda/conda)',
+    )
+    .argument('<project>', 'the project number (e.g. 5)', parseInt)
+    .argument('[issue]', 'the issue id (e.g. XXXXX)', parseInt)
+    .action(async (project: number, issue: number, owner: Owner) => {
+      console.log(await issueInProject(owner, project, issue))
+    })
+
+  if (argv) program.parseAsync(argv, { from: 'user' })
+  else program.parseAsync()
+}
+
+if (require.main === module) {
+  try {
+    cli()
+  } catch (err) {
+    if (err instanceof Error) setFailed(err)
+    else throw err
+  }
+}
