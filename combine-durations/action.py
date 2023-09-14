c2e60a1639d4f4c473dfa1a77da893f23e846f9d
Ken Odegard <kodegard@anaconda.com>
2023-09-13 21:12:07 -0500

    Improve removing old durations

--- combine-durations/action.py
+++ combine-durations/action.py
@@ -1,10 +1,14 @@
 """Combine test durations from all recent runs."""
 from __future__ import annotations
 
-from argparse import ArgumentParser, ArgumentTypeError
 import json
+from argparse import ArgumentParser, ArgumentTypeError
 from pathlib import Path
 from statistics import fmean
+
+from rich.console import Console
+
+print = Console(color_system="standard", soft_wrap=True).print
 
 
 def validate(value: str) -> Path:
@@ -23,31 +27,39 @@ parser.add_argument("--durations-dir", type=validate, required=True)
 parser.add_argument("--artifacts-dir", type=validate, required=True)
 args = parser.parse_args()
 
+# aggregate new durations
 combined: dict[str, dict[str, list[float]]] = {}
-
-# aggregate all new durations
 for path in args.artifacts_dir.glob("**/*.json"):
-    os = path.stem
-    combined_os = combined.setdefault(os, {})
+    os_combined = combined.setdefault((OS := path.stem), {})
 
-    data = json.loads(path.read_text())
-    for key, value in data.items():
-        combined_os.setdefault(key, []).append(value)
+    new_data = json.loads(path.read_text())
+    for key, value in new_data.items():
+        os_combined.setdefault(key, []).append(value)
 
-# aggregate new and old durations while discarding durations that no longer exist
-for path in args.durations_dir.glob("**/*.json"):
-    os = path.stem
-    combined_os = combined.setdefault(os, {})
+# aggregate old durations
+for path in args.durations_dir.glob("*.json"):
+    try:
+        os_combined = combined[(OS := path.stem)]
+    except KeyError:
+        # KeyError: OS not present in new durations
+        print(f"⚠️ {OS} not present in new durations, removing")
+        path.unlink()
+        continue
 
-    data = json.loads(path.read_text())
-    for key in set(combined_os).intersection(args.durations_dir.glob("**/*.json")):
-        combined_os.setdefault(key, []).append(data[key])
+    old_data = json.loads(path.read_text())
+    if missing := set(old_data) - set(combined[OS]):
+        for name in missing:
+            print(f"⚠️ {OS}::{name} not present in new durations, removing")
 
-# write out averaged durations
-for os, combined_os in combined.items():
-    (args.durations_dir / f"{os}.json").write_text(
+    # only copy over keys that are still present in new durations
+    for key in set(old_data) & set(combined[OS]):
+        os_combined[key].append(old_data[key])
+
+# drop durations no longer present in new durations and write out averages
+for OS, os_combined in combined.items():
+    (args.durations_dir / f"{OS}.json").write_text(
         json.dumps(
-            {key: fmean(values) for key, values in combined_os.items()},
+            {key: fmean(values) for key, values in os_combined.items()},
             indent=4,
             sort_keys=True,
         )
