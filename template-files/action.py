af5f413f98448139cf0b07b9c4c2b95d5aeeb80e
Ken Odegard <kodegard@anaconda.com>
2023-09-18 21:58:22 -0500

    Rework into functions for error handling

--- template-files/action.py
+++ template-files/action.py
@@ -3,12 +3,13 @@ from __future__ import annotations
 
 import os
 import sys
-from argparse import ArgumentParser, ArgumentTypeError
+from argparse import ArgumentParser, ArgumentTypeError, Namespace
 from pathlib import Path
 from typing import Any
 
 import yaml
 from github import Auth, Github, UnknownObjectException
+from github.Repository import Repository
 from jinja2 import Environment, FileSystemLoader
 from jsonschema import validate
 from rich.console import Console
@@ -50,101 +51,136 @@ def validate_dir(value: str) -> Path:
         raise ArgumentTypeError(f"{value} is not a valid directory: {err}")
 
 
-# parse CLI for inputs
-parser = ArgumentParser()
-parser.add_argument("--config", type=validate_file, required=True)
-parser.add_argument("--stubs", type=validate_dir, required=True)
-args = parser.parse_args()
-if not args.config:
-    print("⚠️ No configuration file found, nothing to update")
-    sys.exit(0)
+def parse_args() -> Namespace:
+    # parse CLI for inputs
+    parser = ArgumentParser()
+    parser.add_argument("--config", type=validate_file, required=True)
+    parser.add_argument("--stubs", type=validate_dir, required=True)
+    return parser.parse_args()
 
-# read and validate configuration file
-config = yaml.load(
-    args.config.read_text(),
-    Loader=yaml.SafeLoader,
-)
-validate(
-    config,
-    schema={
-        "type": "object",
-        "patternProperties": {
-            r"\w+/\w+": {
-                "type": "array",
-                "items": {
-                    "type": ["string", "object"],
-                    "minLength": 1,
-                    "properties": {
-                        "src": {"type": "string"},
-                        "dst": {"type": "string"},
-                        "with": {
-                            "type": "object",
-                            "patternProperties": {
-                                r"\w+": {"type": "string"},
+
+def read_config(args: Namespace) -> dict:
+    # read and validate configuration file
+    config = yaml.load(
+        args.config.read_text(),
+        Loader=yaml.SafeLoader,
+    )
+    validate(
+        config,
+        schema={
+            "type": "object",
+            "patternProperties": {
+                r"\w+/\w+": {
+                    "type": "array",
+                    "items": {
+                        "type": ["string", "object"],
+                        "minLength": 1,
+                        "properties": {
+                            "src": {"type": "string"},
+                            "dst": {"type": "string"},
+                            "with": {
+                                "type": "object",
+                                "patternProperties": {
+                                    r"\w+": {"type": "string"},
+                                },
                             },
                         },
+                        "required": ["src"],
                     },
-                    "required": ["src"],
-                },
-            }
+                }
+            },
         },
-    },
-)
+    )
+    return config
 
-# initialize Jinja environment and GitHub client
-env = Environment(
-    loader=FileSystemLoader(args.stubs),
-    # {{ }} is used in MermaidJS
-    # ${{ }} is used in GitHub Actions
-    # { } is used in Python
-    # %( )s is used in Python
-    block_start_string="[%",
-    block_end_string="%]",
-    variable_start_string="[[",
-    variable_end_string="]]",
-    comment_start_string="[#",
-    comment_end_string="#]",
-    keep_trailing_newline=True,
-)
-gh = Github(auth=Auth.Token(os.environ["GITHUB_TOKEN"]))
 
-# iterate over configuration and template files
-errors = 0
-for repository, files in config.items():
-    try:
-        repo = gh.get_repo(repository)
-    except UnknownObjectException as err:
-        perror(f"❌ Failed to fetch {repository}: {err}")
-        errors += 1
-        continue
-
-    for file in files:
-        src: str
-        dst: Path
-        context: dict[str, Any]
-
-        if isinstance(file, str):
-            src = file
-            dst = Path(file)
-            context = {}
-        elif isinstance(file, dict):
-            src = file["src"]
-            dst = Path(file.get("dst", src))
-            context = file.get("with", {})
-
+def iterate_config(config: dict, gh: Github, env: Environment, source: Repository) -> int:
+    # iterate over configuration and template files
+    errors = 0
+    for repository, files in config.items():
         try:
-            content = repo.get_contents(src).decoded_content.decode()
+            destination = gh.get_repo(repository)
         except UnknownObjectException as err:
-            perror(f"❌ Failed to fetch {src} from {repository}: {err}")
+            perror(f"❌ Failed to fetch {repository}: {err}")
             errors += 1
             continue
 
-        template = env.from_string(content)
-        dst.parent.mkdir(parents=True, exist_ok=True)
-        dst.write_text(template.render(repo=repo, **context))
+        for file in files:
+            src: str
+            dst: Path
+            context: dict[str, Any]
 
-        print(f"✅ Templated {repository}/{src} as {dst}")
+            if isinstance(file, str):
+                src = file
+                dst = Path(file)
+                context = {}
+            elif isinstance(file, dict):
+                src = file["src"]
+                dst = Path(file.get("dst", src))
+                context = file.get("with", {})
 
-if errors:
-    perror(f"Got {errors} error(s)")
-sys.exit(errors)
+            try:
+                content = destination.get_contents(src).decoded_content.decode()
+            except UnknownObjectException as err:
+                perror(f"❌ Failed to fetch {src} from {repository}: {err}")
+                errors += 1
+                continue
+
+            # inject stuff about the source and destination
+            context["repo"] = context["dst"] = context["destination"] = source
+            context["src"] = context["source"] = destination
+
+            template = env.from_string(content)
+            dst.parent.mkdir(parents=True, exist_ok=True)
+            dst.write_text(template.render(**context))
+
+            print(f"✅ Templated {repository}/{src} as {dst}")
+
+    return errors
+
+
+def main():
+    errors = 0
+
+    args = parse_args()
+    if not args.config:
+        print("⚠️ No configuration file found, nothing to update")
+        sys.exit(0)
+
+    config = read_config(args)
+
+    # initialize Jinja environment and GitHub client
+    env = Environment(
+        loader=FileSystemLoader(args.stubs),
+        # {{ }} is used in MermaidJS
+        # ${{ }} is used in GitHub Actions
+        # { } is used in Python
+        # %( )s is used in Python
+        block_start_string="[%",
+        block_end_string="%]",
+        variable_start_string="[[",
+        variable_end_string="]]",
+        comment_start_string="[#",
+        comment_end_string="#]",
+        keep_trailing_newline=True,
+    )
+    gh = Github(auth=Auth.Token(os.environ["GITHUB_TOKEN"]))
+
+    # get current repository
+    repository = os.environ["GITHUB_REPOSITORY"]
+    try:
+        source = gh.get_repo(repository)
+    except UnknownObjectException as err:
+        perror(f"❌ Failed to fetch {repository}: {err}")
+        errors += 1
+
+    if not errors:
+        errors += iterate_config(config, gh, env, source)
+
+    if errors:
+        perror(f"Got {errors} error(s)")
+    sys.exit(errors)
+
+
+if __name__ == '__main__':
+    main()
